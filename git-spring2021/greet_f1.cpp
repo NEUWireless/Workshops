2e69680cf4545cad7b7e714ec94d5ecd63345ed8
jleightcap <jleightcap@gmail.com>
2021-02-01 16:50:48 -0500

    clang-format cpp

--- git-spring2021/greet_f1.cpp
+++ git-spring2021/greet_f1.cpp
@@ -2,18 +2,13 @@
 // Adapted from MIT's Missing Semester Lecture 6 (Git)
 // CC BY-NC-SA
 
-#include <string>
 #include <iostream>
+#include <string>
 
-std::string greeting() {
-    return "Hello World!";
-}
+std::string greeting() { return "Hello World!"; }
 
-std::string cat() {
-    return "Meow!";
-}
+std::string cat() { return "Meow!"; }
 
-int main(int ac, char* av[]) {
-    std::cout << greeting() << std::endl
-        << cat() << std::endl;
+int main(int ac, char *av[]) {
+  std::cout << greeting() << std::endl << cat() << std::endl;
 }
--- git-spring2021/greet_f2.cpp
+++ git-spring2021/greet_f2.cpp
@@ -2,18 +2,13 @@
 // Adapted from MIT's Missing Semester Lecture 6 (Git)
 // CC BY-NC-SA
 
-#include <string>
 #include <iostream>
+#include <string>
 
-std::string greeting() {
-    return "Hello World!";
-}
+std::string greeting() { return "Hello World!"; }
 
-std::string dog() {
-    return "Woof!";
-}
+std::string dog() { return "Woof!"; }
 
-int main(int ac, char* av[]) {
-    std::cout << greeting() << std::endl
-        << dog() << std::endl;
+int main(int ac, char *av[]) {
+  std::cout << greeting() << std::endl << dog() << std::endl;
 }
